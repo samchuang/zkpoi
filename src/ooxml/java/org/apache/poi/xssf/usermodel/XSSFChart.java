3b2c8736ef80e580c46b7f8759b8a95d779dadc2
Sam Chuang <qk168899@gmail.com>
2011-12-08 04:52:25 +0000

    fixed bug: when rename sheet, cause NPE

--- src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFChart.java
+++ src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFChart.java
@@ -19,42 +19,13 @@ package org.zkoss.poi.xssf.usermodel;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.xml.namespace.QName;
 
-import org.zkoss.poi.POIXMLDocumentPart;
-import org.zkoss.poi.openxml4j.opc.PackagePart;
-import org.zkoss.poi.openxml4j.opc.PackageRelationship;
-import org.zkoss.poi.util.Internal;
-import org.zkoss.poi.ss.usermodel.Chart;
-import org.zkoss.poi.ss.usermodel.ClientAnchor;
-import org.zkoss.poi.ss.usermodel.charts.CategoryDataSerie;
-import org.zkoss.poi.ss.usermodel.charts.ChartAxis;
-import org.zkoss.poi.ss.usermodel.charts.ChartAxisFactory;
-import org.zkoss.poi.ss.usermodel.charts.ChartDirection;
-import org.zkoss.poi.ss.usermodel.charts.ChartType;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFBar3DChartData;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFBarChartData;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFCategoryAxis;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFChartDataFactory;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFChartAxis;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFColumn3DChartData;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFColumnChartData;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFDoughnutChartData;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFLine3DChartData;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFLineChartData;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFPie3DChartData;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFPieChartData;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFValueAxis;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFManualLayout;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFChartLegend;
-import org.zkoss.poi.xssf.usermodel.charts.XSSFView3D;
-import org.zkoss.poi.ss.usermodel.charts.ChartData;
-import org.zkoss.poi.ss.usermodel.charts.AxisPosition;
 import org.apache.xmlbeans.XmlException;
 import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlOptions;
@@ -70,31 +41,52 @@ import org.openxmlformats.schemas.drawingml.x2006.chart.CTDoughnutChart;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTLine3DChart;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTLineChart;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTOfPieChart;
+import org.openxmlformats.schemas.drawingml.x2006.chart.CTPageMargins;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTPie3DChart;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTPieChart;
+import org.openxmlformats.schemas.drawingml.x2006.chart.CTPlotArea;
+import org.openxmlformats.schemas.drawingml.x2006.chart.CTPrintSettings;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTRadarChart;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTScatterChart;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTStockChart;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTSurface3DChart;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTSurfaceChart;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTTitle;
-import org.openxmlformats.schemas.drawingml.x2006.chart.CTTx;
-import org.openxmlformats.schemas.drawingml.x2006.chart.CTView3D;
-import org.openxmlformats.schemas.drawingml.x2006.chart.ChartSpaceDocument;
-import org.openxmlformats.schemas.drawingml.x2006.chart.CTLayout;
-import org.openxmlformats.schemas.drawingml.x2006.chart.CTManualLayout;
-import org.openxmlformats.schemas.drawingml.x2006.chart.CTPlotArea;
 import org.openxmlformats.schemas.drawingml.x2006.chart.CTValAx;
-import org.openxmlformats.schemas.drawingml.x2006.chart.CTPrintSettings;
-import org.openxmlformats.schemas.drawingml.x2006.chart.CTPageMargins;
+import org.openxmlformats.schemas.drawingml.x2006.chart.ChartSpaceDocument;
 import org.openxmlformats.schemas.drawingml.x2006.chart.STBarDir;
-import org.openxmlformats.schemas.drawingml.x2006.chart.STLayoutTarget;
-import org.openxmlformats.schemas.drawingml.x2006.chart.STLayoutMode;
-import org.openxmlformats.schemas.drawingml.x2006.main.CTRegularTextRun;
-import org.openxmlformats.schemas.drawingml.x2006.main.CTTextParagraph;
 import org.openxmlformats.schemas.officeDocument.x2006.relationships.STRelationshipId;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
+import org.zkoss.poi.POIXMLDocumentPart;
+import org.zkoss.poi.openxml4j.opc.PackagePart;
+import org.zkoss.poi.openxml4j.opc.PackageRelationship;
+import org.zkoss.poi.ss.usermodel.Chart;
+import org.zkoss.poi.ss.usermodel.ClientAnchor;
+import org.zkoss.poi.ss.usermodel.charts.AxisPosition;
+import org.zkoss.poi.ss.usermodel.charts.CategoryDataSerie;
+import org.zkoss.poi.ss.usermodel.charts.ChartAxis;
+import org.zkoss.poi.ss.usermodel.charts.ChartAxisFactory;
+import org.zkoss.poi.ss.usermodel.charts.ChartData;
+import org.zkoss.poi.ss.usermodel.charts.ChartTextSource;
+import org.zkoss.poi.ss.usermodel.charts.ChartType;
+import org.zkoss.poi.util.Internal;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFBar3DChartData;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFBarChartData;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFCategoryAxis;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFChartAxis;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFChartDataFactory;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFChartLegend;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFColumn3DChartData;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFColumnChartData;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFDoughnutChartData;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFLine3DChartData;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFLineChartData;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFManualLayout;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFPie3DChartData;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFPieChartData;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFValueAxis;
+import org.zkoss.poi.xssf.usermodel.charts.XSSFView3D;
 
 /**
  * Represents a SpreadsheetML Chart
@@ -451,7 +443,9 @@ public final class XSSFChart extends POIXMLDocumentPart implements Chart, ChartA
     //20111007, henrichen@zkoss.org: rename CategoryDataSerie
     private void renameSheet(List<? extends CategoryDataSerie> series, String oldname, String newname) {
 		for (CategoryDataSerie serie : series) {
-			serie.getTitle().renameSheet(oldname, newname);
+			ChartTextSource title = serie.getTitle();
+			if (title != null)
+				title.renameSheet(oldname, newname);
 			serie.getCategories().renameSheet(oldname, newname);
 			serie.getValues().renameSheet(oldname, newname);
 		}
